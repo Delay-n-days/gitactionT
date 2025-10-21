60e690b64f4c9836410097219c33142ba9cc805a
hu <you@example.com>
2025-10-21 15:39:23 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World from CMake and GitHub Actions!" << std::endl;
+    std::cout << "Hello World from CMake and GitHub Actions! 222" << std::endl;
     return 0;
 }
